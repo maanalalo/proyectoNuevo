8e68db885043053303e9929adcd87644fe6354dd
maanalalo <maanalalo@gmail.com>
2016-06-17 14:07:33 +0200

    Primer commit

---
+++ app/src/main/java/net/intergrupo/academia/otroproyectofernando/OtroProyectoActivity.java
@@ -0,0 +1,13 @@
+package net.intergrupo.academia.otroproyectofernando;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OtroProyectoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otro_proyecto);
+    }
+}
